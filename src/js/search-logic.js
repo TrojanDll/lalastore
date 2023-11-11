5b40093deac6cd0730be4f60f24ce55757f0853d
TrojanDll <dzafarovt36@gmail.com>
2023-11-11 20:09:53 +0300

    feat: advertising adaptation added; subheader adaptation added

--- src/js/search-logic.js
+++ src/js/search-logic.js
@@ -5,6 +5,12 @@
   const searchAreaResults = document.querySelector(
     ".header__search__area__results"
   );
+  const searchAreaMobile = document.querySelector(
+    ".input.header__sidebar-search__search__area"
+  );
+  const searchAreaResultsMobile = document.querySelector(
+    ".header__sidebar-search__results"
+  );
 
   searchInput.addEventListener("focus", (e) => {
     search.classList.add("header__search_focused");
@@ -16,5 +22,9 @@
     search.classList.remove("header__search_focused");
     searchArea.classList.remove("header__search__area_focused");
     searchAreaResults.style.display = "none";
+  });
+
+  searchAreaMobile.addEventListener("focus", (e) => {
+    searchAreaResultsMobile.style.display = "flex";
   });
 })();
--- src/js/sidebar-logic.js
+++ src/js/sidebar-logic.js
@@ -1,9 +1,42 @@
 (function () {
   const toggle = document.querySelector(".nav-toggle");
-  const sidebar = document.querySelector(".header__sidebar");
+  const sidebar = document.querySelector(".header__sidebar-menu");
+  const body = document.querySelector("body");
+  const searchBtn = document.querySelector(".header-mobile__search-btn");
+  const sidebarSearch = document.querySelector(".header__sidebar-search");
 
   toggle.addEventListener("click", function (e) {
-    this.classList.toggle("opened");
-    sidebar.classList.toggle("header__sidebar_opened");
+    if (
+      (sidebar.classList.contains("header__sidebar_opened") &&
+        !sidebarSearch.classList.contains("header__sidebar_opened")) ||
+      (!sidebar.classList.contains("header__sidebar_opened") &&
+        !sidebarSearch.classList.contains("header__sidebar_opened"))
+    ) {
+      sidebar.classList.toggle("header__sidebar_opened");
+      body.classList.toggle("overflow-hidden");
+      this.classList.toggle("opened");
+    }
+
+    if (
+      !sidebar.classList.contains("header__sidebar_opened") &&
+      sidebarSearch.classList.contains("header__sidebar_opened")
+    ) {
+      toggle.classList.toggle("opened");
+      sidebarSearch.classList.toggle("header__sidebar_opened");
+      body.classList.toggle("overflow-hidden");
+    }
+  });
+
+  searchBtn.addEventListener("click", function (e) {
+    if (
+      (!sidebar.classList.contains("header__sidebar_opened") &&
+        !sidebarSearch.classList.contains("header__sidebar_opened")) ||
+      (!sidebar.classList.contains("header__sidebar_opened") &&
+        sidebarSearch.classList.contains("header__sidebar_opened"))
+    ) {
+      toggle.classList.toggle("opened");
+      sidebarSearch.classList.toggle("header__sidebar_opened");
+      body.classList.toggle("overflow-hidden");
+    }
   });
 })();
